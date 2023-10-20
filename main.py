334eab5a925e7564caf08f3486b42939a88072e0
Vasanth P <vasanthpoobalan38@gmail.com>
2023-10-20 23:27:57 +0530

    Initial Commit GPA NEC IT

--- main.py
+++ main.py
@@ -1,7 +1,59 @@
 import streamlit as st
 st.title("GPA Calc")
 
-subcredit={"Probaility and Statistics":3, "Data Structures using Python": 4, "OOPS using Java": 3, "Computer Architecture and Organization":3, "Digital Electronics and Microprocessor":3, "OOPS using Java Laboratory":2, "Digital Electronics and Microprocessor Laboratory":1}
+subcredit={
+     "Professional English- I": 3,
+     "Calculus and Solid Geometry": 4,
+     "Physics for Computer Engineers": 4,
+     "Environmental Science": 3,
+     "Python Programming": 3,
+     "Engineering Graphics Laboratory": 2,
+     "Python Programming Laboratory": 2,
+
+     "Professional English- II": 3,
+     "Complex Analysis and Laplace Transforms": 4,
+     "Applied Physics": 3,
+     "Chemistry for Computer Engineers": 4,
+     "Structured Programming": 3,
+     "Basics of Electronics Engineering": 3,
+     "Structured Programming Laboratory": 2,
+     "Electronics Laboratory": 2,
+
+     "Probaility and Statistics": 3,
+     "Data Structures using Python": 4,
+     "OOPS using Java": 3,
+     "Computer Architecture and Organization": 3,
+     "Digital Electronics and Microprocessor": 3,
+     "OOPS using Java Laboratory": 2,
+     "Digital Electronics and Microprocessor Laboratory": 1,
+
+     "Discrete Mathematics": 3,
+     "Design and Analysis of Algorithms": 4,
+     "Computer Networks and Internets": 3,
+     "Database Management System": 3,
+     "Operating System Principles": 3,
+     "Software Engineering": 3,
+     "Computer Networks and Internets Laboratory": 1,
+     "Database Management System Laboratory": 2,
+     "Operating System Principles Laboratory": 1,
+
+     "Digital Signal Processing": 3,
+     "Internet and Web Programming": 3,
+     "Object Oriented Analysis and Design": 3,
+     "Computer Graphics and Multimedia": 3,
+     "Case Tools Laboratory": 2,
+     "Computer Graphics and Multimedia Laboratory": 1,
+
+     "Artificial Intelligence": 3,
+     "Compiler Design": 3,
+     "Cryptography and Network Security": 3,
+     "Internet of Things Laboratory": 2,
+
+     "Engineering Economcs and Financial Accounting": 3,
+     "Machine Learning Techniques": 3,
+     "Full Stack Development": 3,
+     "Machine Learning Techniques Laboratory": 2
+}
 col1, col2 = st.columns([2, 2])
 sublist=[]
 credlist=[]
@@ -14,14 +66,13 @@ with col1:
         if subject:
             sublist.append(subject)
             credlist.append(subcredit[subject])
-    if len(sublist)==no_of_subjects:
-        st.write(sublist)
-        st.write(credlist)
+#    if len(sublist)==no_of_subjects:
+#        st.write(sublist)
+#        st.write(credlist)
 
 gradeplist=[]
 with col2:
     st.title("Grades Obtained")
-
     for i in range(ns):
         grade = st.text_input(f"Enter Grade Obtained in Sub{i + 1}")
         grade=grade.upper()
@@ -41,8 +92,8 @@ with col2:
             gradepoint = 0
         if grade:
             gradeplist.append(gradepoint)
-    if len(gradeplist) == no_of_subjects:
-        st.write(gradeplist)
+#    if len(gradeplist) == no_of_subjects:
+#        st.write(gradeplist)
 
 
 prolist=[]
